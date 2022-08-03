d724c35ef85864a0caf5e17c7e24f267aa99f360
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-08-03 16:19:49 -0700

    added a toggle class

--- app.js
+++ app.js
@@ -3,20 +3,24 @@
 const contOne = document.querySelector('.cont--one--whole');
 const contTwo = document.querySelector('.cont--two--whole');
 const boxes = document.querySelectorAll('.box');
+const erase = document.querySelector('.button--erase');
 
 
 
 // click numbers 1 to 40
 contOne.addEventListener('click', (e) => {
-        e.target.classList.add('bg-red');
-        
-
-});
+    e.target.classList.toggle('bg-red');
+      
+}); 
 
 // click numbers 41 to 80
 contTwo.addEventListener('click', (e) => {
-    e.target.classList.add('bg-red');
-    
+    e.target.classList.toggle('bg-red');
 
+});
+
+
+erase.addEventListener('click', () => {
+    
 });
 
