37fbdf11f30612827440027782e740845cfacfda
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-08-02 19:14:03 -0700

    added click events to change color of numbers

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 'use strict';
-const contOne = document.querySelector('.cont--one--whole');
-const boxes = document.getElementById('box');
 
-contOne.addEventListener('click', () => {
-    boxes.style.backgroundColor = 'red';
+const contOne = document.querySelector('.cont--one--whole');
+const contTwo = document.querySelector('.cont--two--whole');
+const boxes = document.querySelectorAll('.box');
+
+
+
+// click numbers 1 to 40
+contOne.addEventListener('click', (e) => {
+        e.target.classList.add('bg-red');
+        
+
+});
+
+// click numbers 41 to 80
+contTwo.addEventListener('click', (e) => {
+    e.target.classList.add('bg-red');
+    
 
 });
 
