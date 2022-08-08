ac72def210cc80bfe174b343a6cda0339b8146d8
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-08-08 15:57:14 -0700

    added play functon to generate 6 numbers

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const contTwo = document.querySelector('.cont--two--whole');
 const container = document.querySelectorAll('.container');
 const boxes = document.querySelectorAll('.box');
 const erase = document.querySelector('.button--erase');
+const play = document.querySelector('.button--play');
+let creditAvail = document.querySelector('.credit--available');
+let win = document.querySelector('.win');
+
 
 
 
 // click numbers 1 to 40
 contOne.addEventListener('click', (e) => {
     e.target.classList.toggle('bg-red');
-    
+
 }); 
 
 // click numbers 41 to 80
@@ -21,7 +25,16 @@ contTwo.addEventListener('click', (e) => {
 });
 
 
-erase.addEventListener('click', () => {
-    boxes.classList.toggle('bg-red');
+erase.addEventListener('click', (e) => {
+    e.target.classList.remove('bg-red');
 });
 
+
+play.addEventListener('click', () => {
+    let minm = 1;
+    let maxm = 80;
+    const play = Math.trunc(Math.random() * 80) + 1;
+    console.log(play);
+    
+    
+});
